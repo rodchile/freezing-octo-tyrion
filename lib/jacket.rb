d608095390c7b276fc30537a135a30d9f1174b7e
Rodrigo García S <rod@rodrigogarcia.net>
2012-06-29 19:31:56 -0400

    Fixes DB URL for Heroku

--- lib/jacket.rb
+++ lib/jacket.rb
@@ -10,10 +10,6 @@ require_relative 'accounts/accounts'
 require_relative 'transfers/internal_transfers'
 
 module Jacket  
-  configure :production do
-    ENV['DATABASE_URL'] = "sqlite://#{Dir.pwd}/db/jacket_production.db"
-  end
-  
   configure :development  do
     ENV['DATABASE_URL'] = "sqlite://#{Dir.pwd}/db/jacket_development.db"
   end
